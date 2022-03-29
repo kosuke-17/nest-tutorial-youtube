712b38eac8d5d9f50ca8bcd7af0aa5ab5f8539a0
kosuke-17 <tamutamu17@icloud.com>
2022-03-29 09:20:30 +0900

    リターンの値をオブジェクトに変更

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -81,16 +81,25 @@ export class AuthService {
    * @param email - メールアドレス
    * @returns jwtToken
    */
-  signToken(userId: number, email: string): Promise<string> {
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
     const payload = {
       sub: userId,
       email,
     };
     const secret = this.config.get('JWT_SECRET');
 
-    return this.jwt.signAsync(payload, {
+    const token = await this.jwt.signAsync(payload, {
       expiresIn: '15m',
       secret: secret,
     });
+
+    return {
+      access_token: token,
+    };
   }
+
+  return;
 }
