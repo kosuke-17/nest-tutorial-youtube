d14be55ab1d9ad2f1498add8bffda56722bedc63
kosuke-17 <tamutamu17@icloud.com>
2022-03-29 17:56:14 +0900

    ユーザー情報更新の処理実装

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,23 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Patch, Get, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { JwtGuard } from '../auth/guard';
 import { GetUser } from '../auth/decorator';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Get('me')
   // prismaスキーマに基づいたUserの型
   getMe(@GetUser() user: User) {
     return user;
   }
 
-  // @Patch()
-  // editUser() {}
+  // idに紐ずくユーザー情報の更新
+  @Patch()
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
+  }
 }
