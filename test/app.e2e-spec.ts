8408fb736090ecd0692edba71a7a836cfabe2c45
kosuke-17 <tamutamu17@icloud.com>
2022-03-29 17:08:10 +0900

    新規登録できることのテスト実装

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,9 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
 import { PrismaService } from '../src/prisma/prisma.service';
 import { AppModule } from '../src/app.module';
+import { AuthDto } from 'src/auth/dto/auth.dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -22,16 +24,47 @@ describe('App e2e', () => {
     );
     // アプリの初期化を行う
     await app.init();
+    // アプリの起動
+    await app.listen(9000);
 
     // インスタンス化させる
     prisma = app.get(PrismaService);
     // PrismaServiceで用意したDBをきれいにするメソッドを実装
     await prisma.cleanDB();
+    // 変化しないURLを前もって準備しておく
+    pactum.request.setBaseUrl('http://localhost:9000');
   });
 
   // アプリケーションを閉じる
   afterAll(() => {
     app.close();
   });
-  it.todo('通過');
+
+  describe('認証', () => {
+    describe('新規登録', () => {
+      it('新規登録できること', () => {
+        const dto: AuthDto = { email: 'test@test.com', password: 'test1234' };
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+        // .inspect();
+      });
+    });
+    describe('ログイン', () => {
+      it.todo('ログインできること');
+    });
+  });
+  // describe('ユーザー', () => {
+  //   describe('本人情報取得', () => {});
+  //   describe('ユーザー情報編集', () => {});
+  // });
+  // describe('ブックマークス', () => {
+  //   describe('ブックマークを作成する', () => {});
+  //   describe('ブックマークを取得する', () => {});
+  //   describe('idに紐ずくブックマークを取得する', () => {});
+  //   describe('ブックマークを編集する', () => {});
+  // describe('ブックマークを削除する', () => {});
+  // });
 });
